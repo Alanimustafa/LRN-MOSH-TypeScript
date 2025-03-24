38fdda6afd458d0063fc879318e7a51bc94246c3
MustafaAlani <mustafa.kamel.ani@gmail.com>
2025-03-24 13:05:15 -0500

    Debuggin using F5 and F10 for step-by-step debugging

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,11 @@
 "use strict";
 console.log('Hello World!');
 var age = 30;
-console.log('I am' + age + 'years old.');
+console.log('I am ' + age + ' years old.');
+if (age >= 21) {
+    console.log('You are old enough to drink.');
+}
+else {
+    console.log('You are not old enough to drink.');
+}
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
 console.log ('Hello World!');
 var age : number = 30;
-console.log ('I am'+ age +'years old.');
+console.log ('I am '+ age +' years old.');
+if (age >= 21) { 
+    console.log ('You are old enough to drink.'); 
+} else { 
+    console.log ('You are not old enough to drink.');
+}
