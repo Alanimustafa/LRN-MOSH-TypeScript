1e221bec3abdf3305c93f8779edae32acea59bc7
MustafaAlani <mustafa.kamel.ani@gmail.com>
2025-03-25 22:43:09 -0500

    Objects - Using the readonly modifier

--- dist/objects.js
+++ dist/objects.js
@@ -1,6 +1,8 @@
 "use strict";
-let employee = { id: 1 };
+let employee = { id: 1, name: '' };
 employee.id = 0;
 employee.name = "Mustafa";
+console.log(employee);
+employee.id = 1;
 console.log(employee);
 //# sourceMappingURL=objects.js.map
--- src/objects.ts
+++ src/objects.ts
@@ -1,11 +1,12 @@
 
 let employee : {
-    id?: number,
+    readonly id: number, // Adding the ? means this property is optional.
     // name?: string // name property is optional, that's mean if name has been provided the comiler won't return error. However, it is not a good approach. The better way to declare a default value to the name.
     name: string
 } = { id: 1, name: ''}
 
 employee.id = 0 ;  // id property is optional, so we can assign a value to it.
 employee.name = "Mustafa" ;
-
+console.log (employee);
+employee.id = 1 ; // id property is optional, so we can assign a value to.
 console.log(employee); // Output: { id: 0, name: 'Mustafa' }
